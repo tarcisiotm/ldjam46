662677932901990a4d7cd9b640378b3ae0893c6a
Tarcísio Costa <tarcisiotm@gmail.com>
2020-04-18 15:49:37 -0700

    feat: ui sprites

---
+++ Assets/Scripts/Gameplay/TreeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    [SerializeField] float water = 100;
+    [SerializeField] float sun = 50;
+    [SerializeField] float health = 100;
+
+    bool isDay = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
